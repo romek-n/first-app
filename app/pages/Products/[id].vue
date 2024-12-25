1074648bc15cef67230e76f456cd2a330f7a67ee
romek-n <7562191@gmail.com>
2024-12-25 17:09:09 +0200

    R

--- app/pages/Products/[id].vue
+++ app/pages/Products/[id].vue
@@ -1,11 +1,11 @@
-<script setup lang="ts">
+<script setup>
   definePageMeta({
     layout: 'products'
   })
 
   const { id } = useRoute().params
   const uri = 'https://fakestoreapi.com/products/' + id
-  const { data: product} = await useFetch(uri)
+  const { data: product} = await useFetch(uri, { key: id})
 </script>
 
 <template>
