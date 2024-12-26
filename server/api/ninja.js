daaa7c5e149990924f85d46100dddccc7a55fe0a
romek-n <7562191@gmail.com>
2024-12-26 16:37:06 +0200

    Не получается подключить api, выдвет ошибку почему то и не отображается на сайте

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,5 +1,18 @@
-export default defineEventHandler(() => {
-   return{
-      message: `Hello, ninjas`
-   }
-})
+export default defineEventHandler(async (event) => {
+  // handle query params
+  // const { name } = useQuery(event)
+
+  // handle post data
+  // const { age } = await useBody(event)
+
+  // return {
+  //   message: `Hello, ${name}! You are ${age} years old.`
+  // }
+
+  // api call with private key
+  const { data } = await $fetch(
+    "https://api.currencyapi.com/v3/latest?apikey=cur_live_I1ptXb76cimapHat8hecXiTPc52sa7nn07QQblsX"
+  );
+
+  return data;
+});
