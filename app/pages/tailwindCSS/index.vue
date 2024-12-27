d0b8089497854770d5294e65ff909876560b42ac
romek-n <7562191@gmail.com>
2024-12-27 16:51:08 +0200

    d

---
+++ app/pages/tailwindCSS/index.vue
@@ -0,0 +1,60 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div>
+      <nav>
+        <div>
+          <h1>
+            <a href="/">Food ninja</a>
+          </h1>
+        </div>
+        <ul>
+          <li>
+            <a href="#"></a>
+            <span>Home</span>
+          </li>
+          <li>
+            <a href="#"></a>
+            <span>About</span>
+          </li>
+          <li>
+            <a href="#"></a>
+            <span>Contact</span>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <main>
+      <div>
+        <a href="#">Log in</a>
+        <a href="#">Sign up</a>
+      </div>
+      <header>
+        <h2>Recipes</h2>
+        <h3>For Ninjas</h3>
+      </header>
+
+      <div>
+        <h4>Latest Recipes</h4>
+        <div>
+          <div>
+            <!-- <img src="img/stew.jpg" alt="stew"> -->
+            
+            <div>
+              <span>5 Bean Chilli Stew</span>
+              <span>Recipe by Mario</span>
+            </div>
+          </div>
+        </div>
+        <h4>Most Popular</h4>
+        <div>
+          <div>Load more</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style></style>
